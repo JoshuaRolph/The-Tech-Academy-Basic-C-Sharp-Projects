de6b297085bd27a3e6dfd567864b497572e318c0
JoshuaRolph <jrolph45@gmail.com>
2025-05-05 16:55:14 -0600

    BasicBooleanLogic

---
+++ BooleanLogic.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooleanLogic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine(true && false);
+            // Console.WriteLine(true && true);
+            // Console.WriteLine(false && false);
+
+            // Console.WriteLine(true || true);
+            // Console.WriteLine(true || true);
+            // Console.WriteLine(false || false);
+
+            //Console.WriteLine(true == true);
+            //Console.WriteLine(true == false);
+            //Console.WriteLine(false == false);
+
+            //Console.WriteLine(true != true);
+            //Console.WriteLine(true != false);
+            //Console.WriteLine(false != false);
+
+            //Console.WriteLine(true ^ true);
+            //Console.WriteLine(true ^ false);
+            //Console.WriteLine(false ^ false);
+
+            //Console.WriteLine("Magnum Car Insurance Company");
+            //Console.WriteLine("What is your age?");
+            //int age = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Have you ever had a DUI? (Yes or No)");
+            //string answer = Console.ReadLine().ToLower();
+            //bool dui = answer == "Yes";
+            //Console.WriteLine("How many speeding tickets do you have?");
+            //int tickets = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Qualified for Insurance?");
+            //bool qualified = (age > 15) && !dui && (tickets <= 3);
+            //Console.WriteLine("Qualified");
+
+            //Console.ReadLine();
+        }
+    }
+}
