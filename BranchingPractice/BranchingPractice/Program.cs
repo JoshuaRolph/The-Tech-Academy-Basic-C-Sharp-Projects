8e9ed8c8e766f0040eace76d6645a7c2b36c4b7f
JoshuaRolph <jrolph45@gmail.com>
2025-05-08 17:30:40 -0600

    Practice

---
+++ BranchingPractice/BranchingPractice/Program.cs
@@ -0,0 +1,256 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BranchingPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+
+        {
+            //if (true)
+
+            //Console.WriteLine("Hello!");
+            //Console.ReadLine();
+
+            //if (1 == 1)
+            //{
+            //    Console.WriteLine("1 equals 1!");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("1 does not equal 1");
+            //}
+            //Console.ReadLine();
+
+            //string name = "Brett";
+            //if (name == "Jesse")
+            //{
+            //    Console.WriteLine("Your name is Jesse");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Your name is not Jesse");
+            //}
+            //Console.ReadLine();
+
+            //string name = "Adam";
+
+            //if (name == "Brett")
+            //{
+            //    Console.WriteLine("Your name is not Jesse");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Your name is not Jesse. Your name is not Brett");
+            //}
+            //Console.ReadLine();
+
+            //string name = "Erik";
+
+            //if (name == "Jesse")
+            //{
+            //    Console.WriteLine("Your name is Jesse");
+            //}
+            //else if (name == "Brett")
+            //{
+            //    Console.WriteLine("Your name is not Jesse");
+            //}
+            //else if (name == "Adam")
+            //{
+            //    Console.WriteLine("Your name is Adam");
+            //}
+            //Console.ReadLine();
+
+            //string name = "Bilbo";
+
+            //if (name == "Bob")
+            //{
+            //    Console.WriteLine("Your name is George");
+            //}
+            //else if (name == "George")
+            //{
+            //    Console.WriteLine("Your name is definitely not George");
+            //}
+            //Console.ReadLine();
+
+            //string name = "Adam";
+
+            //if (name != "Jesse")
+            //{
+            //    Console.WriteLine("Your name is not Jesse");
+            //}
+
+            //if (1 != 2)
+            //{
+            //    Console.WriteLine("All is right with the universe.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("We live in a bizarro world.");
+            //}
+            //Console.ReadLine();
+
+            //int num1 = 12;
+            //int num2 = 14;
+            //if (num1 > num2)
+            //{
+            //    Console.WriteLine("The first number is larger.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("The second number is larger");
+            //}
+            //Console.ReadLine();
+
+            //int age = 19;
+
+            //if (age < 21)
+            //{
+            //    Console.WriteLine("No, you may not purchase that bottle of wine.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("That will be $23.71");
+            //}
+            //Console.ReadLine();
+
+            //int age = 21;
+
+            //if (age <= 20)
+            //{
+            //    Console.WriteLine("No, you may not purchase this bottle of wine.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("There you go!");
+            //}
+            //Console.ReadLine();
+
+            //int age = 40;
+
+            //if (age <= 60)
+            //{
+            //    Console.WriteLine("Man, You are OLD!");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("You are still old!");
+            //}
+            //Console.ReadLine();
+
+            //int num1 = 7;
+            //int num2 = 18;
+            //int num3 = 12;
+            //if (num2 > num1 && num3 < num2)
+            //{
+            //    Console.WriteLine("The second number is larger than the others.");
+            //}
+            //Console.ReadLine();
+
+            //int num1 = 7;
+            //if (true && true && true && true && num1 == 20)
+            //{
+            //    Console.WriteLine("Everything was true!");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("There was a false somewhere!");
+            //}
+
+            //int packageWeight = 5;
+            //int packageLength = 25;
+            //int packageHeight = 10;
+            //int packageWidth = 5;
+            //if (packageWeight + packageLength + packageHeight + packageWidth > 50)
+            //{
+            //    Console.WriteLine("This package is too big to ship.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("This package is good to go!");
+            //}
+
+            //int num1 = 7;
+
+            //if (num1 < 7 || num1 == 12)
+            //{
+            //    Console.WriteLine("At least one value is true.");
+            //}
+
+            //if (num1 == 12 || num1 == 18)
+            //{
+            //    Console.WriteLine("At least one value is true.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("No value is true.");
+            //}
+
+            //if (num1 == 12 || num1 == 18 || num1 == 20 || num1 == 7)
+            //{
+            //    Console.WriteLine("Something is true.");
+            //}
+
+            //string role = "admin";
+            //if (role == "admin" || role == "administrator")
+            //{
+            //    Console.WriteLine("You may have access to the entire software system.");
+            //}
+
+            //int num1 = 7;
+            //if (num1 < 18 && ((num1 == 7) || num1 == 2) && ((num1 == 8 && num1 > 20) || (num1 < 20 && num1 > 2)))
+            //{
+            //    Console.WriteLine("Wow, that was confusing.");
+            //}
+
+            //int num1 = 7;
+            //int num2 = 12;
+            //string result = num1 > num2 ? "num1 is greater than num2" : "num1 is not greater than num2";
+            //Console.WriteLine(result);
+
+            //int currentTemperature = 80;
+            //int roomTemperature = 70;
+            //string comparisonResult = currentTemperature == roomTemperature ? "It's room temp." : "It is not room temp.";
+            //Console.WriteLine(comparisonResult);
+
+            //    int roomTemperature = 70;
+
+            //    Console.WriteLine("Hi, what is your name?");
+            //    string name = Console.ReadLine();
+
+            //    Console.WriteLine("Hi, " + name + ", what is the temperature where you are?");
+            //    int currentTemperature = Convert.ToInt32(Console.ReadLine());
+
+            //    if (roomTemperature == currentTemperature)
+            //    {
+            //        Console.WriteLine("It is exactly room temperature");
+            //    }
+            //    else if (currentTemperature > roomTemperature)
+            //    {
+            //        Console.WriteLine("It is warmer than room temperature.");
+            //    }
+            //    else if (roomTemperature > currentTemperature)
+            //    {
+            //        Console.WriteLine("It is colder than room temperature.");
+            //    }
+
+            //else
+            //    {
+            //        Console.WriteLine("Uhhhhhh...... something went wrong here...");
+            //    }
+
+            //Console.WriteLine("What is your favorite number?");
+            //int favNum = Convert.ToInt32(Console.ReadLine());
+
+            //string result = favNum == 12 ? "You have an awesome favorite number." : "You do not have an awesome favorite number.";
+            //Console.WriteLine(result);
+
+            //Console.ReadLine();
+        }
+    }
+}
