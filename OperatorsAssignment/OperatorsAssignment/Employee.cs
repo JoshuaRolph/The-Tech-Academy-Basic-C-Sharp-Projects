139cfa0f0a376b7cdd394b2777f172720b9bbb0f
JoshuaRolph <jrolph45@gmail.com>
2025-06-01 17:05:31 -0600

    OperatorAssignment

---
+++ OperatorsAssignment/OperatorsAssignment/Employee.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsAssignment
+{
+    public class Employee
+    {
+        public int Id {get; set;}
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+
+        // Overload "==" Operator, comparing two Employee objects by Id property //
+        public static bool operator ==(Employee emp1, Employee emp2)
+        {
+
+            if (ReferenceEquals(emp1, null) || ReferenceEquals(emp2, null))
+                return ReferenceEquals(emp1, emp2);
+
+            return emp1.Id == emp2.Id;
+
+        }
+
+        // Override Equals and GetHashCode for consistency //
+        public static bool operator !=(Employee emp1, Employee emp2)
+        {
+            return !(emp1 == emp2);
+        }
+        public override bool Equals(object obj)
+        {
+ 
+                
+                if (obj is Employee other)
+                   
+                    return this.Id == other.Id;
+
+            return false;
+            
+        }
+        public override int GetHashCode()
+        {
+            return Id.GetHashCode();
+        }
+    }
+}
---
+++ OperatorsAssignment/OperatorsAssignment/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiating two Employees //
+            Employee employee1 = new Employee() { Id = 1, firstName = "Bob", lastName = "Baganoosh" };
+            Employee employee2 = new Employee() { Id = 2, firstName = "John", lastName = "SaTool" };
+
+            // Comparing the two Employee Objects //
+            if (employee1 == employee2)
+            {
+                Console.WriteLine("The employees are equal.");
+
+            }
+
+            else
+            {
+                Console.WriteLine("The employees are NOT equal.");
+            }
+            // Modifying emp2 to have same Id as emp1 //
+
+            employee2.Id = 1;
+                if (employee1 = employee2)
+            {
+                Console.WriteLine("Employees are NOW equal.");
+            }
+                else
+            {
+                Console.WriteLine("They are still NOT equal.");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
