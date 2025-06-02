cfdd0b4684fe69ab511496eba1d5bdf49766789d
JoshuaRolph <jrolph45@gmail.com>
2025-06-02 17:42:52 -0600

    Parameter

---
+++ Paramater Assignment/Paramater Assignment/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Paramater_Assignment
+{
+    class Employee <T>
+    {
+        public List<T> Id { get; set; }
+    }
+}
---
+++ Paramater Assignment/Paramater Assignment/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Paramater_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee<string> strEmployee = new Employee<string>();
+            Employee<int> intEmployee = new Employee<int>();
+
+            strEmployee.Id = new List<string>() { "John", "Jacob", "Schmidt", "Bob" };
+
+            intEmployee.Id = new List<int>() { "1", "2", "3", "4" };
+
+            foreach (string words in strEmployee.Id)
+            {
+                Console.WriteLine(words);
+            }
+            foreach (int num in intEmployee.Id )
+            {
+                Console.WriteLine(num);
+            }
+        }
+    }
+}
