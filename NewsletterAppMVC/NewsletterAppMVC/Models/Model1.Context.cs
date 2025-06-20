a9d4f4f56e076edd7cde4b156edb26e4aa407599
JoshuaRolph <jrolph45@gmail.com>
2025-06-19 21:08:29 -0600

    Updates

---
+++ NewsletterAppMVC/NewsletterAppMVC/Models/Model1.Context.cs
@@ -0,0 +1,25 @@
+﻿namespace NewsletterAppMVC.Models
+{
+    using NewsletterAppMVC;
+    using NewsletterAppMVC.Models;
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Infrastructure;
+
+    public partial class NewsletterEntities : DbContext
+    {
+        public NewsletterEntities()
+            : base("name=NewsletterEntities")
+        {
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            throw new UnintentionalCodeFirstException();
+        }
+
+        public virtual DbSet<SignUp> SignUps { get; set; }
+
+        
+    }
+}
