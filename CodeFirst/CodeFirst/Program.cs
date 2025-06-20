d5c20604307bb75ed5fb5a5c1c89202f2647aeae
JoshuaRolph <jrolph45@gmail.com>
2025-06-19 22:31:24 -0600

    CodeFirstAssignment

---
+++ CodeFirst/CodeFirst/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CodeFirst
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new SchoolContext())
+            {
+                var stud = new Student() { StudentName = "Josh" };
+
+                db.Students.Add(stud);
+                db.SaveChanges();
+            }
+        }
+    }
+
+
+}
